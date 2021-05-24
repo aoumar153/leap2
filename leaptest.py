1cc57c122f2b6e9ab94ce3f673d91ef93c496938
aoumar153 <63009456+aoumar153@users.noreply.github.com>
2021-05-23 23:32:21 -0700

    Add files via upload

--- leaptest.py
+++ leaptest.py
@@ -9,3 +9,18 @@ def test_p():
 def test_p():
     s = '2340'
     assert leap(s) == 'error'
+
+
+def leap(year):
+    #year = input("Input the year you want to check: ")
+    print(year)
+    if(int(year)% 4 == 0):
+        if(int(year)%100 ==0):
+            if(int(year)% 400 == 0):
+                return 'This is a leap year'
+            else: 
+                return 'not a leap year'
+        else: 
+            return 'This is a leap year'
+    else: 
+        return 'not a leap year'
